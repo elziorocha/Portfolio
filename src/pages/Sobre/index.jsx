428302215c9d0ff7b47ac5464cb5c51baebd4738
elzio <rochasenzo.04@gmail.com>
2023-10-11 16:39:34 -0300

    falta o sobre e os projetos

--- src/pages/Sobre/index.jsx
+++ src/pages/Sobre/index.jsx
@@ -4,34 +4,14 @@ import Header from '../../Components/Header';
 
 function Sobre() {
   return (
-    <div>
-      <div className='background_sobre'>
-      <Header/>
-      
-      <br />
-        <div className="sobre_caixa">
-          <h2>Acesse aos Jogos Instantaneamente</h2>
-          <h3>Encontre entre 500.000 jogos à sua disposição para baixar e se divertir!<br /><br />
-            Conecte-se com seus amigos<br /><br />Aproveite as promoções exclusivas
-          </h3>
-        </div>
-        <br /><br /><Link to="/jogos" className='links_sobre'>Navegue pela loja</Link>
+    <div className='background_sobre'>
+      <Header />
 
-        <div className="sobre_caixa">
-          <h2>Participe da Comunidade</h2>
-          <h3>Conheça novas pessoas, entre em grupos, converse enquanto joga e muito mais!<br /><br />
-            Com mais de 100 milhões de prováveis amigos <br />(ou inimigos), a diversão nunca acaba.</h3>
-        </div>
-        <br /><br /><Link to="*" className='links_sobre'>A Comunidade</Link>
+      <div className="sobre_caixa">
 
-        <div className="sobre_caixa">
-          <h2>Lance o seu jogo</h2>
-          <h3>Tenha a oportunidade de lançar o seu jogo em nosso site!<br/>
-           A Atlantic Games fornece um conjunto de ferramentas e serviços para auxiliar
-           no seu desenvolvimento</h3>
-        </div>
-
-        <br /><br /><Link to="/contato" className='links_sobre'>Nosso Contato</Link>
+        <h2>Quem Sou?</h2>
+        <h3>Sou um estudante cursando o 4º Período de Engenharia de Software
+        </h3>
 
       </div>
     </div>
