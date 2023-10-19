6d17ac68b71b6e7bb89a0802bbfac4c5189cd427
elzio <rochasenzo.04@gmail.com>
2023-10-19 19:02:17 -0300

    falta somente os projetos na aba Portfólio

--- src/pages/Sobre/index.jsx
+++ src/pages/Sobre/index.jsx
@@ -11,21 +11,25 @@ function Sobre() {
 
       <div className="sobre_caixa">
         <h2>Quem Sou?</h2>
-        <h3>Sou um estudante cursando o 4º Período de Engenharia de Software,
+        <h3>Olá! Me chamo Enzo Rocha, sou um estudante cursando o 4º Período de Engenharia de Software,
           na Instituição Faculdade Campo Real
         </h3>
       </div>
 
       <div className="sobre_caixa">
-        <h2>O que busco?</h2>
-        <h3>-icons-
+        <h2>O que busco?</h2> 
+        <h3>Procuro primeira experiência profissional para estagiar em empresas de tecnologia e
+          desenvolvimento de software
         </h3>
       </div>
 
       <div className="sobre_caixa">
-        <h2>Tecnologias Utilizadas</h2>
-        <DiVisualstudio/> <FaHtml5/> <FaCss3Alt/> <FaJsSquare/> <FaReact/> 
-        <br/> <FaNodeJs/> <BiLogoFirebase/> <BiLogoBlender/> <DiMysql/>
+        <h2>Tecnologias utilizadas em projetos</h2>
+        <div className='icons'>
+        <DiVisualstudio className='vscode'/> <FaHtml5 className='html'/> <FaCss3Alt className='css'/> 
+        <FaJsSquare className='js'/> <FaReact className='react'/> 
+        <br/> <FaNodeJs className='node'/> <BiLogoFirebase className='firebase'/> <BiLogoBlender className='blender'/> <DiMysql className='sql'/>
+        </div>
       </div>
 
     </div>
