8b4cab6bee4895f0b01c9b7d77e994ae4b29b309
elzio <rochasenzo.04@gmail.com>
2023-10-27 18:28:20 -0300

    teste

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,14 +7,14 @@ function Home(){
       <div className='background_home'>
         <Header/>
 
-        <section className='texto_home'>
-          <h1 className='texto1'>Enzo Rocha</h1>
-          <h2 className='texto2'>FrontEnd Portfolio</h2>
-        </section>
+          <section className='texto_home'>
+            <h1 className='texto1'>Enzo Rocha</h1>
+            <h2 className='texto2'>FrontEnd Portfolio</h2>
+          </section>
 
-        <section className='foto_home'>
-          <img src={fotoeu} alt='imagem perfil'/>
-        </section>
+          <section className='foto_home'>
+            <img src={fotoeu} alt='imagem perfil'/>
+          </section>
 
       </div>
   );
--- src/pages/Sobre/index.jsx
+++ src/pages/Sobre/index.jsx
@@ -9,43 +9,30 @@ function Sobre() {
     <div className='background_sobre'>
       <Header />
 
-      <ul class="circles">
-                          <li></li>
-                          <li></li>
-                          <li></li>
-                          <li></li>
-                          <li></li>
-                          <li></li>
-                          <li></li>
-                          <li></li>
-                          <li></li>
-                          <li></li>
-                          <li></li>
+      <div className='sobre_container'>
+        <div className="sobre_caixa">
+          <h2>Quem Sou?</h2>
+          <h3>Olá! Me chamo Enzo Rocha, sou um estudante cursando o 4º Período de Engenharia de Software,
+            na Instituição Faculdade Campo Real
+          </h3>
+        </div>
 
-      <div className="sobre_caixa">
-        <h2>Quem Sou?</h2>
-        <h3>Olá! Me chamo Enzo Rocha, sou um estudante cursando o 4º Período de Engenharia de Software,
-          na Instituição Faculdade Campo Real
-        </h3>
-      </div>
+        <div className="sobre_caixa">
+          <h2>O que busco?</h2> 
+          <h3>Procuro primeira experiência profissional para estagiar em empresas de tecnologia e
+            desenvolvimento de software
+          </h3>
+        </div>
 
-      <div className="sobre_caixa">
-        <h2>O que busco?</h2> 
-        <h3>Procuro primeira experiência profissional para estagiar em empresas de tecnologia e
-          desenvolvimento de software
-        </h3>
-      </div>
-
-      <div className="sobre_caixa">
-        <h2>Tecnologias utilizadas em projetos</h2>
-        <div className='icons'>
-        <DiVisualstudio className='vscode'/> <FaHtml5 className='html'/> <FaCss3Alt className='css'/> 
-        <FaJsSquare className='js'/> <FaReact className='react'/> 
-        <br/> <FaNodeJs className='node'/> <BiLogoFirebase className='firebase'/> <BiLogoBlender className='blender'/> <DiMysql className='sql'/>
+        <div className="sobre_caixa">
+          <h2>Tecnologias utilizadas em projetos</h2>
+          <div className='icons'>
+          <DiVisualstudio className='vscode'/> <FaHtml5 className='html'/> <FaCss3Alt className='css'/> 
+          <FaJsSquare className='js'/> <FaReact className='react'/> 
+          <br/> <FaNodeJs className='node'/> <BiLogoFirebase className='firebase'/> <BiLogoBlender className='blender'/> <DiMysql className='sql'/>
+          </div>
         </div>
       </div>
-
-    </ul>
 
     </div>
   );
