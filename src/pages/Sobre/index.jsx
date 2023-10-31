ee18c3a21bcb1a392bd51b7a0dc38226f9f1acf0
elzio <rochasenzo.04@gmail.com>
2023-10-31 17:33:08 -0300

    Sobre reformulado

--- src/pages/Sobre/index.jsx
+++ src/pages/Sobre/index.jsx
@@ -1,6 +1,6 @@
 import './Sobre.css';
 import Header from '../../Components/Header';
-import { FaCss3Alt, FaHtml5, FaJsSquare, FaReact, FaNodeJs } from "react-icons/fa";
+import { FaCss3Alt, FaHtml5, FaJsSquare, FaReact, FaNodeJs, FaBootstrap } from "react-icons/fa";
 import { BiLogoFirebase, BiLogoBlender } from "react-icons/bi";
 import { DiMysql, DiVisualstudio } from "react-icons/di";
 
@@ -9,28 +9,20 @@ function Sobre() {
     <div className='background_sobre'>
       <Header />
 
-      <div className='sobre_container'>
-        <div className="sobre_caixa">
-          <h2>Quem Sou?</h2>
-          <h3>Olá! Me chamo Enzo Rocha, sou um estudante cursando o 4º Período de Engenharia de Software,
-            na Instituição Faculdade Campo Real
-          </h3>
-        </div>
+      <div className="sobre_caixa">
+        <h2>Sobre mim</h2>
+        <h3>Olá! Me chamo Enzo Rocha, sou um estudante cursando o 4º Período de Engenharia de Software,
+          na Instituição Faculdade Campo Real.<br />Gosto muito de estudar sobre Desenvolvimento Web, sendo
+          a minha área preferida no ramo da tecnologia!!!
+        </h3>
+      </div>
 
-        <div className="sobre_caixa">
-          <h2>O que busco?</h2> 
-          <h3>Procuro primeira experiência profissional para estagiar em empresas de tecnologia e
-            desenvolvimento de software
-          </h3>
-        </div>
-
-        <div className="sobre_caixa">
-          <h2>Tecnologias utilizadas em projetos</h2>
-          <div className='icons'>
-          <DiVisualstudio className='vscode'/> <FaHtml5 className='html'/> <FaCss3Alt className='css'/> 
-          <FaJsSquare className='js'/> <FaReact className='react'/> 
-          <br/> <FaNodeJs className='node'/> <BiLogoFirebase className='firebase'/> <BiLogoBlender className='blender'/> <DiMysql className='sql'/>
-          </div>
+      <div className="sobre_caixa">
+        <h2>Tecnologias utilizadas em projetos</h2>
+        <div className='icons'>
+          <DiVisualstudio className='vscode' /> <FaHtml5 className='html' /> <FaCss3Alt className='css' />
+          <FaBootstrap className='bootstrap'/> <FaJsSquare className='js' /> <FaReact className='react' />
+          <br /> <FaNodeJs className='node' /> <BiLogoFirebase className='firebase' /> <BiLogoBlender className='blender' /> <DiMysql className='sql' />
         </div>
       </div>
 
