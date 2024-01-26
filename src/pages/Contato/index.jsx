61e260ba2d96826b463b5e4c9d871f81b86ae104
elziorocha <rochasenzo.04@gmail.com>
2024-01-26 19:21:17 -0300

    ajustes contato e home

--- src/pages/Contato/index.jsx
+++ src/pages/Contato/index.jsx
@@ -3,6 +3,19 @@ import { FaGithubSquare, FaLinkedin, FaWhatsappSquare, FaEnvelopeSquare } from '
 import Header from '../../Components/Header';
 
 function Contato() {
+
+  const handleEmailClick = () => {
+    const email = 'rochasenzo.04@gmail.com';
+    const mailtoLink = `mailto:${email}`;
+
+    window.open(mailtoLink, '_blank');
+  }
+
+  const handleWhatsappClick = () => {
+    const numeroWhatsapp = '5542984018839';
+    const whatsappLink = `https://wa.me/${numeroWhatsapp}`;
+    window.open(whatsappLink, '_blank');
+  };
   return (
     <div className='background_contato'>
       <Header />
@@ -10,12 +23,12 @@ function Contato() {
       <div className='container_contato'>
         <div className='wrapper'>
 
-          <div className='icon email'>
+          <div onClick={handleEmailClick} className='icon email'>
             <h2 className='texto_icone'>rochasenzo.04@gmail.com</h2>
             <FaEnvelopeSquare/>
           </div>
 
-          <div className='icon zap'>
+          <div onClick={handleWhatsappClick} className='icon zap'>
             <h2 className='texto_iconezap'>(42) 98401-8839</h2>
             <FaWhatsappSquare/>
           </div>
