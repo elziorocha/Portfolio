37280d1bc7493bc3018860973bd0dcc0ad9f224d
elziorocha <rochasenzo.04@gmail.com>
2024-03-28 20:01:25 -0300

    update nos ícones da página sobre

--- src/pages/Sobre/index.jsx
+++ src/pages/Sobre/index.jsx
@@ -1,8 +1,8 @@
 import './Sobre.css';
 import Header from '../../Components/Header';
 import fotoeu2 from '../../Assets/eu2.png';
-import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaAngular, FaNodeJs, FaDatabase, FaPhp, FaPython  } from "react-icons/fa6";
-import { BiLogoFirebase } from "react-icons/bi";
+import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaAngular, FaNodeJs, FaDatabase, FaPhp, } from "react-icons/fa6";
+import { BiLogoFirebase, BiLogoRedux, BiLogoMongodb } from "react-icons/bi";
 
 function Sobre() {
   return (
@@ -11,9 +11,9 @@ function Sobre() {
 
       <div className="sobre_caixa">
         <h2>Sobre mim</h2>
-        <img src={fotoeu2} alt='imagem perfil'/>
+        <img src={fotoeu2} alt='imagem perfil' />
         <h3>Olá! Me chamo Enzo Rocha, tenho 19 anos e sou um estudante de Engenharia de Software cursando
-          o 5º Período na Instituição Faculdade Campo Real.<br /><br/>
+          o 5º Período na Instituição Faculdade Campo Real.<br /><br />
           Gosto muito de estudar sobre Desenvolvimento Web, sendo a minha área preferida no ramo da tecnologia!!!
         </h3>
       </div>
@@ -25,13 +25,13 @@ function Sobre() {
       </div>
 
       <div className="sobre_caixa">
-        <h3>Competências: <br/><br/>
-        - Projetos pessoais e acadêmicos desenvolvidos utilizando diversos recursos para desenvolver páginas
-        e aplicações dos mais variados tipos.<br/>
-        - Noções intermediárias de SQL Workbench e manuseio de banco de dados.<br/>
-        - Inglês intermediário, boa noção para leitura e escrita.<br/>
-        - Conhecimento em design para páginas web juntamente com noções de IHC. <br/>
-        - Tenho uma ótima comunicação e desenvolvo rapidamente um bom relacionamento com colegas de grupo!
+        <h3>Competências: <br /><br />
+          - Projetos pessoais e acadêmicos desenvolvidos utilizando diversos recursos para desenvolver páginas
+          e aplicações dos mais variados tipos.<br />
+          - Noções intermediárias de SQL Workbench e manuseio de banco de dados.<br />
+          - Inglês intermediário, boa noção para leitura e escrita.<br />
+          - Conhecimento em design para páginas web juntamente com noções de IHC. <br />
+          - Tenho uma ótima comunicação e desenvolvo rapidamente um bom relacionamento com colegas de grupo!
         </h3>
       </div>
 
@@ -39,9 +39,9 @@ function Sobre() {
         <h2>Tecnologias utilizadas</h2>
         <div className='icons'>
           <FaHtml5 className='html' /> <FaCss3Alt className='css' />
-          <FaJs className='js' /> <FaReact className='react' /> <FaAngular className='angular'/>
+          <FaJs className='js' /> <FaReact className='react' /> <BiLogoRedux className="redux" />  <FaAngular className='angular' />
           <br /> <FaNodeJs className='node' /> <BiLogoFirebase className='firebase' /> <FaDatabase className='dados' />
-          <FaPhp className='php'/> <FaPython className='python' />
+          <BiLogoMongodb className="mongo" /><FaPhp className='php' />
         </div>
       </div>
 
